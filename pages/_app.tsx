465355c66b27121ca828fde50137bd68003a1a2d
Jonathan Cottrell <cottrell.jonathan@gmail.com>
2023-01-15 10:59:59 -0800

    Add initial work in progress landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Oswald } from "@next/font/google";
+
+const font = Oswald({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,11 +16,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta
           name="description"
-          content="The home of Johnny WEHO. A West Hollywood local entrpreneur."
+          content="The home of Johnny WEHO. A perpetual nomad who finally put down roots in West Hollywood."
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Component className={font.className} {...pageProps} />
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
 import React from "react";
+import watercolorImg from "../public/images/watercolor.png";
+import { SocialIcon } from "react-social-icons";
 
 export default function Home() {
   return (
     <main>
-      <p className="text-3xl text-blue-600">Johnny WEHO</p>
+      <div className="h-screen w-screen relative max-w-full">
+        <Image
+          className="w-full h-full object-cover absolute"
+          src={watercolorImg}
+          alt="Johnny WEHO walking across a rainbow crosswalk"
+        />
+        <div className="absolute w-full h-full flex items-center justify-center">
+          <div className="bg-white bg-opacity-40 rounded-lg p-4 flex flex-col gap-4 items-center justify-center drop-shadow-xl">
+            <p className="font-bold text-3xl">Johnny WEHO</p>
+            <div className="flex row">
+              <SocialIcon url="https://instagram.com/johnnyweho" />
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
