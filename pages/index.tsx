88f9e0113fc1e7769e6cdcd33e8745c3834198b3
Jonathan Cottrell <cottrell.jonathan@gmail.com>
2023-01-15 13:52:06 -0800

    Add some text

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,15 +18,28 @@ export default function Home() {
           src={watercolorImg}
           alt="Johnny WEHO walking across a rainbow crosswalk"
         />
-        <div className="absolute w-full h-full flex items-center justify-center">
-          <div className="bg-white bg-opacity-40 rounded-2xl p-8 flex flex-col gap-4 items-center justify-center drop-shadow-xl">
+        <div className="absolute w-full h-full flex items-center justify-center text-center">
+          <div className="bg-white bg-opacity-50 rounded-2xl p-8 m-8 flex flex-col gap-8 items-center justify-center drop-shadow-xl">
             <p
               className={`md:text-5xl lg:text-6xl text-4xl ${titleFont.className}`}
             >
               JOHNNY WEHO
             </p>
+            <p className="text-slate-600 leading-7 max-w-lg">
+              <span className="font-bold">West Hollywood</span>
+              {
+                ", what a town! Looking for some fun and inspiring content about living in West Hollywood?"
+              }
+            </p>
+            <p className="text-slate-600 leading-7 max-w-lg text-lg">
+              <span className="font-bold">Follow me</span>
+              {" and let's explore this beautiful town together!"}
+            </p>
             <div className="flex row">
-              <SocialIcon url="https://instagram.com/johnnyweho" />
+              <SocialIcon
+                fgColor="white"
+                url="https://instagram.com/johnnyweho"
+              />
             </div>
           </div>
         </div>
